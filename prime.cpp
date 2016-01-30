13eab2db220f05cbb7f30e4b73a50f03b72c403a
Prashant Dhirendra <prashant.dhiru@gmail.com>
2016-01-30 16:01:27 +0530

    CPP file added
    main CPP file

---
+++ prime.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+#include<vector>
+#include <math.h>
+using namespace std;
+
+typedef long LL;
+vector<LL> primeList;
+
+int sqrtf(LL);
+int main(){
+	
+	register LL lb=10000000,ub=20000000,i,j;
+	register int sqrt = sqrtf(ub),size,count=0;
+	
+	for(i=2;i<=sqrt;i++){		
+		for(j=2;j<i;j++)
+			if(i%j==0)
+				break;
+		
+		if(j == i ){
+			primeList.push_back(i);			
+		}							
+	}
+	
+	size = primeList.size();
+	for(i=lb;i<=ub;i++){
+		for(j=0;j<size;j++)
+			if(i%primeList[j] == 0 )
+				break;
+		
+		if(j == size ){
+		//printf("%ld\n",i);
+		count++;
+		}							
+	}
+	
+	printf("%d",primeList.size());
+return 0;
+}
+
+int sqrtf(LL n){
+	int i =1;
+	while(i*i<=n){
+		i++;
+	}
+	return i;
+}
